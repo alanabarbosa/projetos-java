7de619bd9b9f36e661df20b47cb95999bd7bbf7d
Alana Barbosa <itsalanayall@hotmail.com>
2024-02-07 13:42:31 -0300

    Create repo java projects

---
+++ src/main/java/com/mycompany/projetosjava/ProjetosJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetosjava;
+
+/**
+ *
+ * @author alanabarbosa
+ */
+public class ProjetosJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
