7386fa7b9d9c908f674036acb472616ae094c066
Alana Barbosa <itsalanayall@hotmail.com>
2024-02-27 21:45:53 -0300

    Add new java file

---
+++ src/main/java/Metodos.java
@@ -0,0 +1,16 @@
+/**
+ * Metodos sem parametro e sem retorno
+ * 
+ * @author Alana Barbosa - Desenvolvedora Front End
+ * @since 07/01/2024 
+ */
+public class Metodos {
+    public void welcome() {
+        System.out.println("Olá, estou testando métodos sem parâmetros e sem retono usando Java!");
+    }
+    
+    public static void main(String[] args) {
+        Metodos methods = new Metodos();
+        methods.welcome();
+    }
+}
