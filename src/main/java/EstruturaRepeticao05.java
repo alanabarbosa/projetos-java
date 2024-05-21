419b3979ad2d72f4ff3b4c9e3ea258f4417f8d4d
Alana Barbosa <itsalanayall@hotmail.com>
2024-05-21 14:51:06 -0300

    Exercicios java

---
+++ src/main/java/EstruturaRepeticao05.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * EstruturaRepeticao06
+ * 
+ * @author Alana Barbosa - Desenvolvedora Front End
+ * @since 07/01/2024 
+ */
+public class EstruturaRepeticao05 {
+    public static void main(String[] args) {
+        
+    }
+}
